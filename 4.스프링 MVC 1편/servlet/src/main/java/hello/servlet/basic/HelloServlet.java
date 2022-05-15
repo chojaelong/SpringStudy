0e838839c1869d536d7db9f1935af43aae66ec3a
JaeHo.Cho <chojaelong@gmail.com>
2022-05-15 16:03:37 +0900

    [2022-05-15][JaeHo.Cho] 서블릿 까지 완료

--- 4.스프링 MVC 1편/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ 4.스프링 MVC 1편/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -10,8 +10,17 @@ import java.io.IOException;
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
 
         System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
     }
 }
