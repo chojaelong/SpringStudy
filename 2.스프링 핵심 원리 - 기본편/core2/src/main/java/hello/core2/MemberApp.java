1747e605b10676f1f2c392f63e06c27f0bccf097
JaeHo.Cho <chojaelong@gmail.com>
2023-03-26 23:07:13 +0900

    [2023-03-26][JaeHo.Cho] 스프링 핵심 원리 기본편 복습 중

---
+++ 2.스프링 핵심 원리 - 기본편/core2/src/main/java/hello/core2/MemberApp.java
@@ -0,0 +1,26 @@
+package hello.core2;
+
+import hello.core2.member.Grade;
+import hello.core2.member.Member;
+import hello.core2.member.MemberService;
+import hello.core2.member.MemberServiceImpl;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigReactiveWebApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
---
+++ 2.스프링 핵심 원리 - 기본편/core2/src/main/java/hello/core2/discount/DiscountPolicy.java
@@ -0,0 +1,15 @@
+package hello.core2.discount;
+
+import hello.core2.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     *
+     * @param member
+     * @param price
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+
+}
