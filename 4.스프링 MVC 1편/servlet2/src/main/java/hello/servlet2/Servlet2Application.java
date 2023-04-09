22e6bc7bc7a7b6776893cfd7fb1606854e473141
JaeHo.Cho <chojaelong@gmail.com>
2023-04-09 22:42:15 +0900

    [2023-04-10][JaeHo.Cho] 스프링 MVC 1편 복습 시작 / 서블릿 완료

--- 4.스프링 MVC 1편/servlet2/src/main/java/hello/servlet2/Servlet2Application.java
+++ 4.스프링 MVC 1편/servlet2/src/main/java/hello/servlet2/Servlet2Application.java
@@ -2,7 +2,9 @@ package hello.servlet2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class Servlet2Application {
 
---
+++ 4.스프링 MVC 1편/servlet2/src/main/java/hello/servlet2/domain/member/MemberRepository.java
@@ -0,0 +1,9 @@
+package hello.servlet2.domain.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemberRepository {
+
+    private Map<Long, Member> store = new HashMap<>();
+}
