319a4cb622cf02587cb0afd796031c1840afeff0
JaeHo.Cho <chojaelong@gmail.com>
2023-04-09 20:06:00 +0900

    [2023-04-09][JaeHo.Cho] 스프링 MVC 1편 복습 시작 / 서블릿 프로젝트 생성

---
+++ 4.스프링 MVC 1편/servlet2/src/main/java/hello/servlet2/Servlet2Application.java
@@ -0,0 +1,13 @@
+package hello.servlet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servlet2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Servlet2Application.class, args);
+	}
+
+}
